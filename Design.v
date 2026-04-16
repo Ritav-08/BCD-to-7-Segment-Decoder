716ccb0c59644bfefcfa0b11a14d0b9179cf7b5e
Ritav <ritav08092004@gmail.com>
2026-04-16 09:30:04 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.04.2026 16:24:08
-// Design Name: 
-// Module Name: BCDt7Sdecoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module BCDt7Sdecoder(
    input [3:0]a_i, 
